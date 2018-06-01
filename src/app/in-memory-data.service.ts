7d29f5bcfe02e7cfece7a086970f51da5b00558d
Denmuch <36898907+Denmuch@users.noreply.github.com>
2018-06-01 07:50:01 +0300

    Update in-memory data

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,7 +8,7 @@ export class InMemoryDataService implements InMemoryDbService {
                     name: 'Название книги 1',
                     author: 'Автор 1',
                     year: '1990',
-                    date: '2018-05-31',
+                    date: '2018-05-01',
                     comment: 'Комментарий',
 
                 },
@@ -17,7 +17,7 @@ export class InMemoryDataService implements InMemoryDbService {
                     name: 'Название книги 2',
                     author: 'Автор 2',
                     year: '1921',
-                    date: '2018-05-31',
+                    date: '2018-05-28',
                     comment: 'Комментарий 2',
 
                 },
@@ -26,7 +26,7 @@ export class InMemoryDataService implements InMemoryDbService {
                     name: 'Название книги 3',
                     author: 'Автор 3',
                     year: '1921',
-                    date: '2018-05-31',
+                    date: '2018-05-29',
                     comment: 'Комментарий 3',
 
                 },
@@ -35,8 +35,17 @@ export class InMemoryDataService implements InMemoryDbService {
                     name: 'Название книги 4',
                     author: 'Автор 4',
                     year: '1923',
-                    date: '2018-05-31',
+                    date: '2018-05-30',
                     comment: 'Комментарий 4',
+
+                },
+                {
+                    id: 5,
+                    name: 'Название книги 5',
+                    author: 'Автор 5',
+                    year: '1999',
+                    date: '2018-05-31',
+                    comment: 'Комментарий 5',
 
                 }
             ];
